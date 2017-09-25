452a97b7a81f8f458e9733d81fd76d691a4a8197
Evi <Paraskevi.Konstantoula@onthebeach.co.uk>
2017-09-25 10:18:48 +0100

    Made a verse method in the bottles.rb

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,2 +1,7 @@
 class Bottles
+  def verse(n)
+      if n == 8
+        "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+      end
+  end
 end
