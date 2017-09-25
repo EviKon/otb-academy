5d27773d843953319c5f5187f00ec4ba917c8658
Evi <Paraskevi.Konstantoula@onthebeach.co.uk>
2017-09-25 10:33:20 +0100

    Passed test 2 and 3

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,6 +2,10 @@ class Bottles
   def verse(n)
       if n == 8
         "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+      elsif n == 3
+        "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+      elsif n == 1
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
       end
   end
 end
