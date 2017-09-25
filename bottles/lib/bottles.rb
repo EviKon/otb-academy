a1d5ab1cb1f15d6537315b8b2fc6a218bbf28626
Evi <Paraskevi.Konstantoula@onthebeach.co.uk>
2017-09-25 12:20:17 +0100

    Passed the tests

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,15 +1,37 @@
 class Bottles
   def verse(n)
-      if n == 8
-        "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
-      elsif n == 3
-        "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
-      elsif n == 1
-        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      # if n == 8
+      #   "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+      # elsif n == 3
+      #   "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
+      # elsif n == 1
+      #   "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      # elsif n == 2
+      #   "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      if n > 2
+        "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottles of beer on the wall.\n"
       elsif n == 2
-        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+        "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down and pass it around, #{n-1} bottle of beer on the wall.\n"
       elsif n == 0
         "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      else
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
       end
   end
+
+  def verses(start, stop)
+     n = start
+     output = []
+     while (n >= stop)
+       output << verse(n) << "\n"
+       n = n-1
+    end
+    output.join
+  end
+
+  def sing
+    verses(99,0)
+  end
+
+
 end
